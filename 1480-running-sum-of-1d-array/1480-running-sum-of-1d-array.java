e64b446ff1d0bd86ba2d565e0786f0c337165fdf
Pradip Kisku <246137392+Pradeeeeeeeep@users.noreply.github.com>
2026-08-13 07:52:27 +0530

    Time: 0 ms (100%), Space: 44.4 MB (17.29%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,12 +1,7 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int[] cNum = nums.clone();
-        for(int i=1; i<nums.length; i++){
-            int tempSum = 0;
-            for(int j=0; j<i; j++){
-                tempSum += cNum[j];
-            }
-            nums[i] += tempSum;
+        for(int i = 1; i < nums.length; i++){
+            nums[i] += nums[i-1];
         }
         return nums;
     }
