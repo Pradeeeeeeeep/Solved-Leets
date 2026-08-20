791e9cb17c745915b6c4a09b8aca41a3008a09dd
Pradip Kisku <246137392+Pradeeeeeeeep@users.noreply.github.com>
2026-08-20 20:46:33 +0530

    Time: 1 ms (89.45%), Space: 43.2 MB (62.12%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int upper = 0;
+        for(int i=0; i<word.length(); i++){
+            if(Character.isUpperCase(word.charAt(i))){
+                upper++;
+            }
+        }
+        if(upper == word.length() || upper == 0 || (upper == 1 && Character.isUpperCase(word.charAt(0)))){
+            return true;
+        }
+        return false;
+    }
+}
