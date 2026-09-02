7f4247ac9565487c415b892216dc6a5483b7ec59
Pradip Kisku <246137392+Pradeeeeeeeep@users.noreply.github.com>
2026-09-02 15:33:49 +0530

    Time: 2 ms (99.36%), Space: 20.8 MB (18.59%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,11 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        txt = re.sub(r'[^a-zA-Z0-9]', '', s)
-        txt = txt.lower()
-        l, r = 0, len(txt)-1
-        while l<=r:
-            if txt[l]!=txt[r]:
-                return False
-            l+=1
-            r-=1
-        return True
+        txt = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        rev_txt = txt[::-1]
+        if txt == rev_txt:
+            return True
+        else:
+            return False
