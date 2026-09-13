0695bc880e7dc54751ac0aa6c39f0620a68b6181
Pradip Kisku <246137392+Pradeeeeeeeep@users.noreply.github.com>
2026-09-14 02:46:41 +0530

    Time: 179 ms (85.38%), Space: 20.1 MB (6.27%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.py
+++ 0835-image-overlap/0835-image-overlap.py
@@ -1,23 +1,26 @@
 class Solution:
     def largestOverlap(self, img1, img2):
-        n = len(img1)
-        ans = 0
+        shape1 = []
+        shape2 = []
+        rows, cols = len(img1), len(img1[0])
+        res = 0
 
-        for dr in range(-(n - 1), n):
-            for dc in range(-(n - 1), n):
+        for i in range(rows):
+            for j in range(cols):
+                if img1[i][j] == 1:
+                    shape1.append((i, j))
 
-                overlap = 0
 
-                for i in range(n):
-                    for j in range(n):
+        for i in range(rows):
+            for j in range(cols):
+                if img2[i][j] == 1:
+                    shape2.append((i, j))
 
-                        ni = i + dr
-                        nj = j + dc
 
-                        if 0 <= ni < n and 0 <= nj < n:
-                            if img1[i][j] == 1 and img2[ni][nj] == 1:
-                                overlap += 1
+        dic = defaultdict(int)
 
-                ans = max(ans, overlap)
+        for x1, y1 in shape1:
+            for x2, y2 in shape2:
+                dic[(x2 - x1, y2 - y1)] += 1
 
-        return ans
+        return max(dic.values()) if dic.values() else 0
