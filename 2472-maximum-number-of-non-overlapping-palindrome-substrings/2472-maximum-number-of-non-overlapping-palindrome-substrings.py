a79237faa05ae14f1d3bc0f0560a44728b0c6eff
Pradip Kisku <246137392+Pradeeeeeeeep@users.noreply.github.com>
2026-09-16 05:01:43 +0530

    Time: 3 ms (95.96%), Space: 19.3 MB (72.73%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -1,37 +1,29 @@
 class Solution:
     def maxPalindromes(self, s: str, k: int) -> int:
+        
+        def check(l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+
         n = len(s)
+        ans = 0
+        start = 0
 
-        palindrome = [[False] * n for _ in range(n)]
+        for r in range(k - 1, n):
+            l = r - k + 1
 
-        for length in range(1, n + 1):
-            for i in range(n - length + 1):
-                j = i + length - 1
+            if l >= start and check(l, r):
+                ans += 1
+                start = r + 1
+                continue
+            
+            l = r - k
+            if l >= start and check(l, r):
+                ans += 1
+                start = r + 1
 
-                if length == 1:
-                    palindrome[i][j] = True
-
-                elif length == 2:
-                    palindrome[i][j] = s[i] == s[j]
-
-                else:
-                    palindrome[i][j] = (
-                        s[i] == s[j] and
-                        palindrome[i + 1][j - 1]
-                    )
-
-        dp = [0] * (n + 1)
-
-        for end in range(1, n + 1):
-
-            dp[end] = dp[end - 1]
-
-            for start in range(end - k + 1):
-
-                if palindrome[start][end - 1]:
-                    dp[end] = max(
-                        dp[end],
-                        dp[start] + 1
-                    )
-
-        return dp[n]
+        return ans
